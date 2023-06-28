1fc2cc3a3ada162fa253f26e9e0919041325370c
tegaomare <tegaomare@yahoo.com>
2023-06-27 21:53:20 -0400

    crud added

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -32,4 +32,60 @@ app.get("/dashboard", async (req, res) => {
     res.status(500).send("Internal Server Error");
   }
 });
+// Create Post
+router.get("/post/create", (req, res) => {
+  res.render("create_post");
+});
+
+router.post("/post/create", async (req, res) => {
+  const { title, contents } = req.body;
+  const userId = req.session.userId;
+  try {
+    // Create a new blog post in the database
+    await BlogPost.create({ title, contents, userId });
+    res.redirect("/dashboard");
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+// Update Post
+router.get("/post/edit/:id", async (req, res) => {
+  const postId = req.params.id;
+  try {
+    const post = await BlogPost.findByPk(postId);
+    res.render("edit_post", { post });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+router.post("/post/edit/:id", async (req, res) => {
+  const postId = req.params.id;
+  const { title, contents } = req.body;
+  try {
+    // Update the blog post in the database
+    await BlogPost.update({ title, contents }, { where: { id: postId } });
+    res.redirect("/dashboard");
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+// Delete Post
+router.get("/post/delete/:id", async (req, res) => {
+  const postId = req.params.id;
+  try {
+    // Delete the blog post from the database
+    await BlogPost.destroy({ where: { id: postId } });
+    res.redirect("/dashboard");
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
 module.exports = router;
