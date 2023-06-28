f79288be95865d5344641325bc44ef57b3d47814
tegaomare <tegaomare@yahoo.com>
2023-06-27 21:25:31 -0400

    blogpost table model created

--- models/BlogPost.js
+++ models/BlogPost.js
@@ -1,3 +1,27 @@
 const { Model, DataTypes } = require("sequelize");
 const bcrypt = require("bcrypt");
 const sequelize = require("../config/connection");
+class BlogPost extends Model {
+  /* checkPassword(loginPw) {
+    return bcrypt.compareSync(loginPw, this.password);
+  }*/
+}
+
+BlogPost.init({
+  title: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  contents: {
+    type: DataTypes.TEXT,
+    allowNull: false,
+  },
+  creator: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  createdAt: {
+    type: DataTypes.DATE,
+    defaultValue: DataTypes.NOW,
+  },
+});
