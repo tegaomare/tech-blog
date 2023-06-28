0163e4b00fb0f78f03c61c12b235f8d6daa644ff
tegaomare <tegaomare@yahoo.com>
2023-06-27 21:15:06 -0400

    updated .env & modified server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const session = require("express-session");
 const exphbs = require("express-handlebars");
 const routes = require("./controllers");
-const helpers = require("./utils/helpers");
+const authorize = require("./utils/auth");
 const sequelize = require("./config/connection");
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
