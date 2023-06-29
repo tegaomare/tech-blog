c7b2aad4bcffcc884bea75c4234b41e01c71fb49
tegaomare <tegaomare@yahoo.com>
2023-06-29 09:13:37 -0400

    updated server links

--- server.js
+++ server.js
@@ -1,16 +1,21 @@
+const path = require("path");
 const express = require("express");
 const session = require("express-session");
 const exphbs = require("express-handlebars");
+const helpers = require("./utils/helpers");
 const routes = require("./controllers");
-const sequelize = require("./config/connection");
-const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+const sequelize = require("./config/connection");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
+
 const sess = {
-  secret: "root",
-  cookie: {},
+  secret: "super secret secret",
+  cookie: {
+    maxAge: 24 * 60 * 60 * 1000,
+  },
   resave: false,
   saveUninitialized: true,
   store: new SequelizeStore({
@@ -20,15 +25,20 @@ const sess = {
 
 app.use(session(sess));
 
-app.engine("handlebars");
+const hbs = exphbs.create({ helpers });
+app.engine("handlebars", hbs.engine);
 app.set("view engine", "handlebars");
 
 app.use(express.json());
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log("Now listening"));
+  app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}. http://localhost:${PORT}`);
+  });
 });
