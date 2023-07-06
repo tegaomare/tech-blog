ede6908f46b739c3523fa76b0c861f777321b3c3
tegaomare <tegaomare@yahoo.com>
2023-07-05 21:28:19 -0400

    login javascript added

---
+++ public/js/login.js
@@ -0,0 +1,25 @@
+const loginFormHandler = async (event) => {
+  event.preventDefault();
+
+  const usernameEl = document.querySelector("#username-login");
+  const passwordEl = document.querySelector("#password-login");
+
+  const response = await fetch("/api/user/login", {
+    method: "POST",
+    body: JSON.stringify({
+      username: usernameEl.value,
+      password: passwordEl.value,
+    }),
+    headers: { "Content-Type": "application/json" },
+  });
+
+  if (response.ok) {
+    document.location.replace("/dashboard");
+  } else {
+    alert("Error logging in.");
+  }
+};
+
+document
+  .querySelector("#login-form")
+  .addEventListener("submit", loginFormHandler);
