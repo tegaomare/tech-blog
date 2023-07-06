b78cb086aaa5fa03c916d13a4d37d7bccda366ac
tegaomare <tegaomare@yahoo.com>
2023-07-05 21:21:28 -0400

    connections added

--- models/index.js
+++ models/index.js
@@ -1,6 +1,33 @@
-const BlogPost = require("./Post");
+const User = require("./User");
 const Comment = require("./Comment");
+const Post = require("./Post");
 
-// establish relationships
+User.hasMany(Post, {
+  foreignKey: "userId",
+  onDelete: "CASCADE",
+});
 
-module.exports = { BlogPost, Comment };
+User.hasMany(Comment, {
+  foreignKey: "userId",
+  onDelete: "CASCADE",
+});
+
+Comment.belongsTo(User, {
+  foreignKey: "userId",
+});
+
+Comment.belongsTo(Post, {
+  foreignKey: "postId",
+});
+
+Post.belongsTo(User, {
+  foreignKey: "userId",
+  onDelete: "CASCADE",
+});
+
+Post.hasMany(Comment, {
+  foreignKey: "postId",
+  onDelete: "CASCADE",
+});
+
+module.exports = { User, Comment, Post };
